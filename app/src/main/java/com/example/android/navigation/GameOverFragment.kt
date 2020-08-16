eb9165c41cd1c5ecad8ddb208a4e2a0dd39ebdda
s.arslanov <arsl.sh056@mail.ru>
2020-08-16 13:49:01 +0500

    saving 4

--- app/src/main/java/com/example/android/navigation/GameOverFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameOverFragment.kt
@@ -31,6 +31,10 @@ class GameOverFragment : Fragment() {
         // Inflate the layout for this fragment
         val binding: FragmentGameOverBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_game_over, container, false)
+
+        binding.tryAgainButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_gameOverFragment3_to_gameFragment)
+        }
         return binding.root
     }
 }
